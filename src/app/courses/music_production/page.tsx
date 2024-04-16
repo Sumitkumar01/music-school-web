8de66babe11c8e190f62b402d04c84ba03b7946e
sumit <sk7627577@gmail.com>
2024-04-16 10:40:32 +0530

    update

--- src/app/courses/music_production/page.tsx
+++ src/app/courses/music_production/page.tsx
@@ -10,7 +10,7 @@ function page() {
           <h1 className='text-4xl dark:text-gray-100  font-bold text-center'>Exploring the Fundamentals of Music Production Theory</h1>
         </div>
         <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 mt-4">
-          <p className='text-lg text-justify dark:text-white mb-2'>Welcome to our comprehensive guide on music production theory! Whether you're a budding producer or a seasoned musician looking to enhance your skills, understanding the underlying principles of music production is essential for creating captivating tracks.</p>
+          <p className='text-lg text-justify dark:text-white mb-2'>Welcome to our comprehensive guide on music production theory! Whether you&apos;re a budding producer or a seasoned musician looking to enhance your skills, understanding the underlying principles of music production is essential for creating captivating tracks.</p>
           <p className='text-lg text-justify dark:text-white mb-2'>From mastering sound design to perfecting arrangement techniques, this guide covers it all. Dive into topics like harmony, rhythm, melody, and structure, and discover how each element contributes to crafting compelling compositions.</p>
           <p className='text-lg text-justify dark:text-white mb-2'>Gain insights into the technical aspects of recording, mixing, and mastering, and learn how to harness the power of technology to bring your musical visions to life.</p>
           <p className='text-lg text-justify dark:text-white mb-2'>Our aim is to empower you with the knowledge and tools necessary to unleash your creativity and elevate your productions to new heights. Explore the theory, experiment with different approaches, and embark on an exciting journey of sonic exploration.</p>
